e022575bc3f2a65bdb0ceb56113f7c66ddc74660
Brick McDermott <mikdermit@icloud.com>
2022-01-13 16:29:05 -0500

    Updates

---
+++ precache-manifest.a76a4cff43842c0f1db1ae33335a8b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a87c515b67ea01e348cfa62db0eef90",
+    "url": "/community-blog/index.html"
+  },
+  {
+    "revision": "27f64f1f4aaac79ee2ab",
+    "url": "/community-blog/static/css/main.fc882eb8.chunk.css"
+  },
+  {
+    "revision": "41ce979f726f67ff11df",
+    "url": "/community-blog/static/js/2.ab4bc165.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/community-blog/static/js/2.ab4bc165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f64f1f4aaac79ee2ab",
+    "url": "/community-blog/static/js/main.0f8453c6.chunk.js"
+  },
+  {
+    "revision": "77996e3b0d423b64fb6b",
+    "url": "/community-blog/static/js/runtime-main.7d02e202.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-routing/precache-manifest.ef2997119d5676cf7d4458d75ebf2e77.js"
+  "/community-blog/precache-manifest.a76a4cff43842c0f1db1ae33335a8b8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-routing/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/community-blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
